b545a90c04f6f2637eaac1c0e685e2bbc22e9540
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-10-06 16:10:40 +0530

    fotter Refactored

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,12 @@
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 
 import { Year } from "@/components/year";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="py-4 mt-3 border-t dark:border-white mb-28 md:mt-6">
-      <footer className="flex flex-col items-baseline space-y-3 md:flex-row md:justify-between">
+    <div className="py-2 mt-3 border-t dark:border-white mb-24 md:mt-10">
+      <footer className="w-full flex flex-col items-center space-y-3 md:flex-row md:justify-between md:items-baseline">
         <div className="flex space-x-2 tracking-tight">
           <span className="text-base font-semibold"> Built with:</span>
           <div className="flex items-center space-x-1 text-sm font-normal">
@@ -17,12 +18,22 @@ export const Footer = () => {
             <span>TailwindCss</span>
           </div>
         </div>
-        <div className="flex self-center justify-center space-x-1 text-base font-semibold tracking-tight md:self-baseline ">
+
+        <div className="flex justify-center space-x-1 text-base font-semibold tracking-tighter md:self-baseline text-muted-foreground ">
           <div className="flex items-center mt-4 md:mt-0 space-x-2 ">
-            <span>©Shivaji</span>
-            <span>
-              <Year />
-            </span>
+            <h2 className="capitalize ">
+              <span>
+                © <Year />
+              </span>
+              <Link
+                href="https://shivaji.vercel.app/"
+                target="_blank"
+                className="px-1 hover:underline hover:border-b-blue-600"
+              >
+                Shivaji.
+              </Link>
+              all rights reserved.
+            </h2>
           </div>
         </div>
       </footer>
