829163376efcc74cbee176148c60ab6308d42c74
Kondeti Sivaji <shivajikondeti555@gmail.com>
2025-02-07 23:52:02 +0530

    footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,21 +6,21 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="py-4 mt-6 border-t dark:border-white md:mt-10">
-      <footer className="w-full flex flex-col items-center space-y-3 md:flex-row md:justify-between md:items-baseline">
+      <footer className="w-full flex flex-col items-center space-y-0 md:flex-row md:justify-between md:items-baseline">
         <div className="flex space-x-2 text-muted-foreground tracking-tighter">
-          <span className="text-base font-semibold"> Built with:</span>
-          <div className="flex items-center space-x-1 text-base font-normal">
-            <RiNextjsFill className="w-6 h-6" />
-            <span>Nextjs</span>
+          <span className="text-sm font-semibold"> Built with:</span>
+          <div className="flex items-center space-x-1 text-sm font-normal">
+            <RiNextjsFill className="w-5 h-5" />
+            <span>NextJS</span>
           </div>
-          <div className="flex items-center space-x-1 text-base font-normal">
+          <div className="flex items-center space-x-1 text-sm font-normal">
             <RiTailwindCssFill className="w-6 h-6" />
-            <span>TailwindCss</span>
+            <span>TailwindCSS</span>
           </div>
         </div>
 
-        <div className="flex justify-center space-x-1 text-base font-semibold tracking-tighter md:self-baseline text-muted-foreground ">
-          <div className="flex items-center mt-4 md:mt-0 space-x-2 ">
+        <div className="flex justify-center space-x-0.5 text-sm font-semibold tracking-tighter md:self-baseline text-muted-foreground ">
+          <div className="flex items-center mt-2 md:mt-0 space-x-0.5 ">
             <h2 className="capitalize">
               ©
               <Link
